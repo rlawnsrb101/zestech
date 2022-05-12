6422faba3de01e7a5f512d7926921f60cfdf2018
rlawnsrb101 <rlawnsrb101@naver.com>
2022-05-12 14:34:11 +0900

    print

---
+++ StirngExample.java
@@ -0,0 +1,8 @@
+public class StirngExample {
+    public static void main(String[] args) {
+        System.out.println("안녕");
+        System.out.println('나');
+
+        System.out.println("안녕 나는 야");
+    }
+}
